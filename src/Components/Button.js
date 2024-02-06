f85e3b5cbcfc402cb8a2179b136ca2e05620a30e
Henry Loveday <henrygad.orji@gmail>
2024-02-06 11:07:35 +0100

    version 0.1

---
+++ src/Components/Button.js
@@ -0,0 +1,25 @@
+
+
+const Button = () => {
+  return (
+    <div>
+    {/*   .button{
+    display: block;
+    text-align: center;
+    color: #fff;
+    width: auto;
+    background-color: var(--secondary-color);
+    border: 0;
+    border-radius: 2px;
+    filter: drop-shadow(.5px .5px .5px rgba(0, 0, 0, .5) );
+    text-decoration: none;
+}
+.button:active{
+    background-color: var(--primary-color);
+    color: black;
+} */}
+    </div>
+  )
+}
+
+export default Button
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+  <h1>Hi Henry</h1>
+  )
+}
+
+export default Home
